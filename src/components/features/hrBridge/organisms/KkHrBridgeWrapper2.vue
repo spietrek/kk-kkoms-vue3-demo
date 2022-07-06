72137777f778c88c2709019e845412f645d33ffb
spietrek <spietrek@gmail.com>
2022-07-06 09:31:47 -0400

    update translation

--- src/components/features/hrBridge/organisms/KkHrBridgeWrapper2.vue
+++ src/components/features/hrBridge/organisms/KkHrBridgeWrapper2.vue
@@ -2,9 +2,21 @@
   <div class="hr-bridge-wrapper">
     <div class="tw-stats tw-shadow-2xl">
       <div class="tw-stat">
-        <div class="tw-stat-title">{{ $t('features.hrbridge.organisms.kkhrbridgewrapper2.postsCreatedUsingComposition') }}</div>
+        <div class="tw-stat-title">
+          {{
+            $t(
+              'features.hrbridge.organisms.kkhrbridgewrapper2.postsCreatedUsingComposition',
+            )
+          }}
+        </div>
         <div class="tw-stat-value tw-text-primary">{{ postsCreated }}</div>
-        <div class="tw-stat-desc">{{ $t('features.hrbridge.organisms.kkhrbridgewrapper2.fromLastMonth', { '1': postsPercentageStatus }) }}</div>
+        <div class="tw-stat-desc">
+          {{
+            $t('features.hrbridge.organisms.kkhrbridgewrapper2.fromLastMonth', {
+              '1': postsPercentageStatus,
+            })
+          }}
+        </div>
       </div>
     </div>
   </div>
